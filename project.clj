ad0b467c0a58fad83f91e56b1b3948bcbcef0593
Joseph Parker <joseph.parker.1@gmail.com>
2017-04-01 18:44:02 -0400

    include test paths in project.clj src-paths

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject selfsame/core.logic "0.8.11-SNAPSHOT"
   :description "A logic/relational programming library for Clojure-CLR"
-  :source-paths ["src/main/clojure"]
-  :test-paths ["src/test/clojure"]
+  :source-paths [
+    "src/main/clojure" 
+    "src/test/clojure"]
   :dependencies [])
